46f92f39fa5c8484afc06a183b031efe71cfd704
Ahloi <xiongjian.loi@acddevelopment.com>
2020-05-21 15:42:02 +0800

    Disable HTML Renderer on Shoutem ui as we didn't use it

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ export { HorizontalPager } from './components/HorizontalPager';
 export { LoadingIndicator } from './components/LoadingIndicator';
 export { PageIndicators } from './components/PageIndicators';
 export { default as RichMedia } from './components/RichMedia';
-export { Html } from './html';
-export { SimpleHtml } from './html';
+// export { Html } from './html';
+// export { SimpleHtml } from './html';
 export { ShareButton } from './components/ShareButton';
 // export { LinearGradient } from './components/LinearGradient';
 
